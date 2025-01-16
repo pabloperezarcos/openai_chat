17f26b8f2d1da9323f0d5a33ce79be0404f2c0e3
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-16 14:39:30 -0300

    Update main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,20 @@
+/**
+ * Punto de entrada principal para la aplicación Angular.
+ * Configura y arranca la aplicación utilizando Angular Standalone APIs.
+ */
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+/**
+ * Inicializa la aplicación Angular.
+ * 
+ * Utiliza la configuración definida en `app.config.server.ts` para registrar
+ * los proveedores y configuraciones necesarias al arrancar la aplicación.
+ *
+ * @returns {Promise<ApplicationRef>} Promesa que se resuelve cuando la aplicación se ha inicializado correctamente.
+ */
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
