e7ea874832954107b26e9ec91e55e5acc8864cf9
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-17 13:05:34 -0300

    Create Component Historial

---
+++ src/app/components/historial/historial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial',
+  imports: [],
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.scss'
+})
+export class HistorialComponent {
+
+}
