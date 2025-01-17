f0265f8b2b0756495cb0d0f06a225e24692da084
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-17 13:05:16 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,14 @@ export const routes: Routes = [
 
     {
         path: '',
-        loadComponent: () => import('./components/chat/chat.component').then(c=>c.ChatComponent)
+        loadComponent: () => import('./components/chat/chat.component').then(c => c.ChatComponent)
+    },
+    {
+        path: 'chatbot_openai',
+        loadComponent: () => import('./components/chat/chat.component').then(c => c.ChatComponent)
+    },
+    {
+        path: 'historial',
+        loadComponent: () => import('./components/historial/historial.component').then(c => c.HistorialComponent)
     }
-
 ];
