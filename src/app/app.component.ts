08d6b522936b16a20b13f8c8d6cdc0a2812cb9ad
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-17 13:05:08 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 /**
  * Componente raíz de la aplicación Angular.
  * 
  * Este componente actúa como el punto de entrada principal para la estructura de la aplicación.
  * Define la plantilla base y configura el enrutamiento principal a través de `RouterOutlet`.
  */
-
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-/**
- * Decorador que define el componente raíz.
- * 
- * - `selector`: Nombre del selector utilizado en el HTML (`<app-root>`).
- * - `imports`: Importa el módulo `RouterOutlet` para el manejo de rutas.
- * - `templateUrl`: Define la plantilla HTML asociada.
- * - `styleUrl`: Define los estilos asociados al componente.
- */
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
