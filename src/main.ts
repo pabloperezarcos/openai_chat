62049858c79c98858d7e9486f3ab953ddb5655cd
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2025-01-20 10:27:45 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+//import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, {
-  
-  providers: [
-    ...appConfig.providers,
-    provideHttpClient()
-  ],
-}).catch(err => console.error(err));
+/* bootstrapApplication(AppComponent, {
+
+    providers: [
+      ...appConfig.providers,
+      provideHttpClient()
+    ],
+  }).catch(err => console.error(err)); */
+
+  bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
